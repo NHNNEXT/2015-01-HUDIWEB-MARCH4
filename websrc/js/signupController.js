c0f19d7f2203f26b6311d64f41f5ab2d29f6666b
tminlim <tmin.jc@gmail.com>
2015-05-23 22:15:43 +0900

    step3_reshaped factory tooltip

--- websrc/js/signupController.js
+++ websrc/js/signupController.js
@@ -1,6 +1,6 @@
 (function() {
 	'use strict';
-	march4.app.registerController('signupController', function($scope, $http, $location, UserService) {
+	march4.app.registerController('signupController', function($scope, $http, $location, UserService, ToolTip) {
 		$scope.user ={};
         $scope.signup = signup;
        
@@ -8,23 +8,33 @@
         	$scope.loading = true;
         	UserService.Create($scope.user)
 	        	.then(function(response){
-	        		if(response.status == 200){
-	        			//error
-	        			if(response.data !== ""){
-	                    	alert('you failed!');
-	                        //FlashService.Error(response.message);
-	                        $scope.loading = false;
-	                    
-	                    //success
-	                    } else {
-	                    	var errors = response.data;
-	        				alert('success!');
-	        				//FlashService.Success('Registration successful', true);
-	                        $location.path('/signin');
-	                    }
-	        		}else{
-	        			//do something	
+	        		if(response.success){ //result by type of tootTip
+	        			console.log("success of data response ");
+	        			ToolTip.Success("Registration successful", true);
+                        $location.path('/signin');
+	        		} else{
+	        			ToolTip.Error(response.message);
+	        			$scope.loading = false;
+	        			console.log("error of data response ");
 	        		}
+	        		
+//	        		if(response.status == 200){
+//	        			//error
+//	        			if(response.data !== ""){
+//	                    	alert('you failed!');
+//	                        //tooltip.Error(response.message);
+//	                        $scope.loading = false;
+//	                    
+//	                    //success
+//	                    } else {
+//	                    	var errors = response.data;
+//	        				alert('success!');
+//	        				//tooltip.Success('Registration successful', true);
+//	                        $location.path('/signin');
+//	                    }
+//	        		}else{
+//	        			//do something	
+//	        		}
 	        	});
         } 
 	});
